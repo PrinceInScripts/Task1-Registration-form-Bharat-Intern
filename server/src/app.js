2f0eb06a7e6d61e0a9bad0d439e3804457aa39a3
princekumar <princepk7878kumar@gmail.com>
2024-04-01 01:27:04 +0530

    done set up of app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,16 @@
-import connectDB from "./db"
+import express from 'express';
+import cors from 'cors';    
+import cookieParser from 'cookie-parser';
 
-dotenv.config({
-    path:"./env"
-})
+const app=express();
 
-connectDB()
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}));
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export {app}
