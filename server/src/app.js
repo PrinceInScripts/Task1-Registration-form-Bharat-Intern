5a0b679728fbdd8d9f5f5239db52c49eb6511510
princekumar <princepk7878kumar@gmail.com>
2024-04-01 01:34:57 +0530

    done error and multer middlrewares.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import cors from 'cors';    
 import cookieParser from 'cookie-parser';
+import { errorHandler } from './middlewares/error.middlewares.js';
+import morgan from "morgan"
+import passport from 'passport';
+import session from "express-session"
 
 const app=express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    // origin:'http://localhost:3000',
+    origin:process.env.CORS_ORIGIN || process.env.FRONTEND_URL,
     credentials:true,
 }));
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(morgan("dev"))
 
+app.use(errorHandler)
 export {app}
