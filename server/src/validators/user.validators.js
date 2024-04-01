dc495cde8b5b9366c5b2587baa05e5a95888af40
princekumar <princepk7878kumar@gmail.com>
2024-04-01 18:30:34 +0530

    done user validators

---
+++ server/src/validators/user.validators.js
@@ -0,0 +1,37 @@
+import {body} from "express-validator"
+
+const userRegisterValidators=()=>{
+    return [
+        body("email")
+                    .trim()
+                    .notEmpty()
+                    .withMessage("Email is required")
+                    .isEmail()
+                    .withMessage("Email is not valid"),
+        body("username")
+                    .trim()
+                    .notEmpty()
+                    .withMessage("Username is required")
+                    .isLowercase()
+                    .withMessage("Username must be lowercase")
+                    .isLength({min:3,max:20})
+                    .withMessage("Username must be between 3 and 20 characters"),
+        body("fullName")
+                    .trim()
+                    .notEmpty()
+                    .withMessage("fullName is required"),
+        body("password")
+                    .trim()
+                    .notEmpty()
+                    .withMessage("Password is required"),
+        body("confirmPassword")
+                    .trim()
+                    .notEmpty()
+                    .withMessage("Confirm Password is required"),
+        
+    ]
+}
+
+export {
+    userRegisterValidators
+}
