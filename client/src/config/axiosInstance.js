5d32e307858845b779297c9938c875da92d63333
princekumar <princepk7878kumar@gmail.com>
2024-04-03 22:15:05 +0530

    done

--- client/src/config/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL="/api/v1/"
-const axiosInstance=axios.create()
-
-axiosInstance.defaults.baseURL=BASE_URL;
-
-export default axiosInstance;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,19 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Provider } from 'react-redux'
-import { ToastContainer } from 'react-toastify'
-import store from './redux/store.js'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-      <BrowserRouter>
-
   <React.StrictMode>
     <App />
-    <ToastContainer/>
-  </React.StrictMode>
-  </BrowserRouter>
-  </Provider>,
+  </React.StrictMode>,
 )
