2b4f2461693ffc08190c6260b4ddfa3f49fa6239
princekumar <princepk7878kumar@gmail.com>
2024-04-10 15:39:39 +0530

    done

--- client/src/page/ResetPassword/ResetPassword.jsx
+++ client/src/page/ResetPassword/ResetPassword.jsx
@@ -44,7 +44,7 @@ function ResetPassword() {
     );
 
     if (response?.payload?.success) {
-    //   navigate("/login");
+      navigate("/login");
     setData({
         password: "",
         confirmPassword: "",
